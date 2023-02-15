457e8804f6c3685995ccf186a10a64a21847e455
root <nf9600372@gmail.com>
2023-02-15 08:35:55 -0800

    [task 4-puts.c]

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,2 +1,10 @@
-#!/bin/bash
-puts ("\Programming is like building a multilingual puzzle\") return (0)
+#include <stdio.h>
+/**
+*main - entry point
+*Return: Always 0 (success)
+*/
+int main(void)
+{
+    puts("\"Programming is like building a multilingual puzzle\n");
+    return (0);
+}
