46944924c28ef2ce89c549b78d09d48a3fd0510e
root <nf9600372@gmail.com>
2023-02-15 10:17:33 -0800

    [task 6-size.c]

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -5,11 +5,11 @@
 */
 int main(void)
 {
-printf("Size of char: %zu bytes(s)\n", sizeof(char));
-printf("Size of int: %zu bytes(s)\n", sizeof(int));
-printf("Size of long int: %zu bytes(s)\n", sizeof(long));
-printf("Size of long long int: %zu bytes(s)\n", sizeof(long long int));
-printf("Size of float: %zu bytes(s)\n", sizeof(float));
+printf("Size of a char: %zu bytes(s)\n", sizeof(char));
+printf("Size of an int: %zu bytes(s)\n", sizeof(int));
+printf("Size of a long int: %zu bytes(s)\n", sizeof(long));
+printf("Size of a long long int: %zu bytes(s)\n", sizeof(long long int));
+printf("Size of a float: %zu bytes(s)\n", sizeof(float));
 
 return (0);
 }
