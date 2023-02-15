81a1f6d8eefd0725be804c2f62624e651e7c2799
root <nf9600372@gmail.com>
2023-02-15 09:07:11 -0800

    [task 5-printf.c]

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - Start
- * 
- * Return: Always 0 (Success)
- */
-int main(void)
+*main - start
+*Return: Always 0 (Success)
+*/
+int
+main(void)
 {
 pritnf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
