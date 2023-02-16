698f9bea349bb4477d71a5ea769eeb4a46eb27d2
root <nf9600372@gmail.com>
2023-02-16 07:22:08 -0800

    [task 2-print_alphabet]

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - start
+ * Return: always 0
+ *
+ *
+ */
+int main(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+{
+	putchar(10);
+	putchar(x);
+}
+	return (0);
+}
