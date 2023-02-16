adcfadcbfa14ccd4ab032ddcddc1bcba094f1d0b
root <nf9600372@gmail.com>
2023-02-16 15:16:07 -0800

    [task 9-print_comb.c]

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ * Return: 0
+ *
+ *
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 48; x < 58; x++)
+{
+	putchar(x);
+	if (x < 57)
+	{
+	putchar(44);
+	putchar(32);
+	}
+}
+	putchar('\n');
+	return (0);
+}
+
