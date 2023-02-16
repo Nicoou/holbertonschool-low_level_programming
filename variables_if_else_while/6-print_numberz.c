c08e56bdb6b1bb4167de09189cc03a4ecc78ab22
root <nf9600372@gmail.com>
2023-02-16 11:33:34 -0800

    [task 6-print_numberz]

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - start
+ * Return: always 0
+ *
+ *
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 48; x <= 57; x++)
+{
+	putchar(x);
+}
+	putchar('\n');
+
+	return (0);
+}
