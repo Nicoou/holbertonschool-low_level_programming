880e1a517f83dfbbdf6ccdcf7f9fe369cf1b3a39
root <nf9600372@gmail.com>
2023-02-16 13:40:41 -0800

    [task print_base16.c]

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - start
+ * Return: always 0
+ *
+ *
+ */
+int main(void)
+{
+	int x;
+	int y;
+	
+	for (x =48; x <= 102; x++)
+	{
+	puchar(x);
+	}
+	for(y =97; y <= 102; y++)
+
+	{
+	puchar (y);
+	}
+	 puchar('\n');
+
+	 return (0);
+}
