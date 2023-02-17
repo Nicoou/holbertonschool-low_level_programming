5b86086fdacd8981741161157a73bc4762d01503
root <nf9600372@gmail.com>
2023-02-17 12:19:01 -0800

    [task6-abs]

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _putchar - writes the character c to stdout
+ * @x: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int
+_abs(int x)
+{
+	if (x < 0)
+	{
+		x = x *  -1;
+		return (x);
+	}
+	else
+	{
+		return (x);
+	}
+}
