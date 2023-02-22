40924fad21b659550c1ce7d0104ead172c78e310
root <nf9600372@gmail.com>
2023-02-22 10:08:58 -0800

    task 3

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * mul - check the code.
+ *@a : variable
+ *@b : variable
+ * Return: Always 0
+ */
+
+int
+mul(int a, int b)
+{
+	int mul;
+
+	mult = a * b;
+	return (mul);
+}
+
