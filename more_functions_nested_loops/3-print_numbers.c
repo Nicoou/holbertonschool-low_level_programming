5eac85b92041295ee67c70e390fd406a2c6ad92d
root <nf9600372@gmail.com>
2023-02-22 11:00:25 -0800

    task 3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _putchar - check the code.
+ *
+ * Return: Always 0
+ */
+void
+print_numbers(void)
+{
+	int num = 48;
+
+		while (num <= 57)
+		{
+			_putchar(num);
+			num++;
+		}
+
+	_putchar('\n');
+}
