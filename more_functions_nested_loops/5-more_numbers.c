69297d3b74c17664fc14f179709e52a77b144049
root <nf9600372@gmail.com>
2023-02-22 12:46:35 -0800

    task 5

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void
+more_numbers(void)
+{
+	int num;
+	int var = 0;
+	int i;
+	int l;
+
+	for (num = 0; num <= 10; num++)
+	{
+		var = 0;
+		while (var <= 14)
+		{
+		i = var % 10;
+		l = var / 10;
+		if (l != 0)
+		{
+			_putchar(l + '0');
+		}
+		_putchar(i + '0');
+		var++;
+		}
+		_putchar('\n');
+	}
+}
