5c95fc416a3eb6040568b1bb40062db2d1f51a59
root <nf9600372@gmail.com>
2023-02-23 07:19:08 -0800

    task 10

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_triangle - Write a function that draws a diagonal.
+ *
+ *@size: number
+ *
+ * Return: Always 0
+ */
+
+void print_triangle(int size)
+{
+	int t, s, n;
+
+	if (size <= 0)
+	{
+		putchar('\n');
+	}
+
+	for (t = 0; t < size; t++)
+	{
+		for (s = size - 1; s > t; s--)
+		{
+			_putchar(' ');
+		}
+		for (n = 0; n <= t; n++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}	
+	
+}
