16dafe7850bfc341b22d24f522f2a094fb8b3684
root <nf9600372@gmail.com>
2023-02-27 11:23:56 -0800

    task 5

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+void
+rev_string(char *s)
+{
+	int i, len, temp;
+
+	len = strlen(s);
+
+	for (i = 0; i < len / 2; i++)
+	{
+
+	temp = s[i];
+	s[i] = s[len - i - 1];
+	s[len - i - 1] = temp;
+	}
+}
