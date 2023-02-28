61825f97ad856bfddae79f7a127f7b75efd90cd7
root <nf9600372@gmail.com>
2023-02-28 06:55:31 -0800

    task 8

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+void
+print_array(int *a, int n)
+{
+	int b;
+
+	for (b = 0; b < n - 1; b++)
+	{
+		printf("%d, ", a[b]);
+	}
+
+	if (b == n - 1)
+	{
+		printf("%d\n", a[b]);
+	}
+}
