07b87db9852326682c9e743d1bd471822e6ac3e8
root <nf9600372@gmail.com>
2023-03-01 11:58:35 -0800

    task 6

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+char
+*cap_string(char *s)
+{
+	int la = 0;
+
+	while (s[la])
+	{
+
+		if (s[la] >= 97 && s[la] <= 122)
+		{
+			switch (s[la - 1])
+			{
+				case 32:
+				case 44:
+				case 59:
+				case 46:
+				case 10:
+				case 9:
+				case 33:
+				case 63:
+				case 34:
+				case 40:
+				case 41:
+				case 123:
+				case 125:
+
+					s[la] = s[la] - 32;
+			}
+		}
+		la++;
+	}
+	return (s);
+}
