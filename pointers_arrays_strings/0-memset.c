224374b33e759e11565b1310007849780934b20a
root <nf9600372@gmail.com>
2023-03-02 12:23:56 -0800

    clean

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,7 +1,16 @@
 #include "main.h"
 
-char
-*_memset(char *s, char b, unsigned int n)
+/**
+ * _memset - Fills the first n bytes of the memory area pointed to
+ * by s with the constant byte b
+ * @s : the address of memory to print
+ * @b : constant byte
+ * @n : bytes of the memory area pointed to by @s
+ *
+ * Return: a pointer to the memory area s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int a;
 
