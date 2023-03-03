a03b159a35a74a052657ac3b7a39566257700212
root <nf9600372@gmail.com>
2023-03-03 07:28:52 -0800

    copies the string pointed

--- pointers_arrays_strings/1-tasks/9-strcpy.c
+++ pointers_arrays_strings/1-tasks/9-strcpy.c
@@ -1,7 +1,15 @@
 #include "main.h"
 
-char
-*_strcpy(char *dest, char *src)
+/**
+ * _strcpy - Copies the string pointed to by src,
+ * including the terminating null byte (\0), to the buffer pointed to by dest
+ * @src : source of my data
+ * @dest : final destiny where I will print my data
+ *
+ * Return: *dest
+ */
+
+char *_strcpy(char *dest, char *src)
 {
 	int ana = 0;
 
