f1760db9d8cc91b2b914d1cf01df0c233507745b
root <nf9600372@gmail.com>
2023-03-03 12:54:07 -0800

    copies a string

--- pointers_arrays_strings/2-tasks/2-strncpy.c
+++ pointers_arrays_strings/2-tasks/2-strncpy.c
@@ -1,7 +1,15 @@
 #include "main.h"
 
-char
-*_strncpy(char *dest, char *src, int n)
+/**
+ * _strncpy - Concatenates two strings
+ * @src : source of my data
+ * @dest : final destiny where I will print my data
+ * @n : quantity of chars to be printed
+ *
+ * Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
 {
 	int laura;
 
