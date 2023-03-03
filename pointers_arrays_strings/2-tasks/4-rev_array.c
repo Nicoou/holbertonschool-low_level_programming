3d4260906a2ea84f743ca71af98ea3ecffe54bd9
root <nf9600372@gmail.com>
2023-03-03 12:57:17 -0800

    reverses the content of an array of integers

--- pointers_arrays_strings/2-tasks/4-rev_array.c
+++ pointers_arrays_strings/2-tasks/4-rev_array.c
@@ -1,7 +1,12 @@
 #include "main.h"
 
-void
-reverse_array(int *a, int n)
+/**
+ * reverse_array - Reverses the content of an array of integers
+ * @a: array
+ * @n: number of elements of the array to swap
+ */
+
+void reverse_array(int *a, int n)
 {
 	int rev = 0, ana = 0;
 	int laura = n - 1;
