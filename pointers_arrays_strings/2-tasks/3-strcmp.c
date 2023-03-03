ba8665ffbe49f93a976423bf30aa7f0c47294aef
root <nf9600372@gmail.com>
2023-03-03 12:55:41 -0800

    compares two strings

--- pointers_arrays_strings/2-tasks/3-strcmp.c
+++ pointers_arrays_strings/2-tasks/3-strcmp.c
@@ -1,7 +1,14 @@
 #include "main.h"
 
-int
-_strcmp(char *s1, char *s2)
+/**
+ * _strcmp - Compares two strings
+ * @s1 : string 1
+ * @s2 : string 2
+ *
+ * Return: diff between s2 and s2
+ */
+
+int _strcmp(char *s1, char *s2)
 {
 	while (*s1 && *s2)
 	{
