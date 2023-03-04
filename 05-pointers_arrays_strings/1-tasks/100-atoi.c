3d8c519fa8cab8094dc40c24eee9871739167373
root <nf9600372@gmail.com>
2023-03-04 13:59:50 -0800

    task 10

---
+++ 05-pointers_arrays_strings/1-tasks/100-atoi.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h
+>
+/**
+ * _atoi - Converts a string to an integer
+ *
+ * @s: *char
+ * Return: converted int
+ */
+
+int _atoi(char *s)
+{
+	printf("Hello World!")
+}
