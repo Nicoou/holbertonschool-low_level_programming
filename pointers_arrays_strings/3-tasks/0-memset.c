71bd05e814cabb12b2622d0664aca36ef58c7692
root <nf9600372@gmail.com>
2023-03-04 12:51:09 -0800

    fills memory with a constant byte

--- pointers_arrays_strings/3-tasks/0-memset.c
+++ pointers_arrays_strings/3-tasks/0-memset.c
@@ -7,7 +7,7 @@
  * @b : constant byte
  * @n : bytes of the memory area pointed to by @s
  *
- * Return: a pointer to the memory area s
+ * Return: a pointer to the memory area s.
  */
 
 char *_memset(char *s, char b, unsigned int n)
