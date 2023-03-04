94064766ba769ce8fb49aee46ab768296402ede2
root <nf9600372@gmail.com>
2023-03-04 12:39:15 -0800

    copies memory area

--- pointers_arrays_strings/3-tasks/1-memcpy.c
+++ pointers_arrays_strings/3-tasks/1-memcpy.c
@@ -1,7 +1,15 @@
 #include "main.h"
 
-char
-*_memcpy(char *dest, char *src, unsigned int n)
+/**
+ * _memcpy - Copies n bytes from memory area src to memory area dest
+ * @dest : destination memory area
+ * @src : source memory area
+ * @n : quantity of bytes to copy
+ *
+ * Return: pointer to dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int a;
 
