0f2e4c9721fa6ce3f9ab14ed9f1c5c65c39555d5
root <nf9600372@gmail.com>
2023-03-08 10:07:05 -0800

    print name argc argv

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *main -  entry point
+ *@argc:int
+ *@argv:char
+ *Return: retunrs 0
+ */
+
+int main (int argc, char *argv[])
+{
+	if (argc >= 0)
+	{
+
+	printf("%s\n", argv[0]);
+
+	return (0);
+	
+	}
+	return (1);
+
+}
