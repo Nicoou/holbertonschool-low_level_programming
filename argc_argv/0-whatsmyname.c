73a01457256a3745bbfefc8a1898e7681c51d991
root <nf9600372@gmail.com>
2023-03-08 10:09:29 -0800

    print name argc argv

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -7,7 +7,7 @@
  *Return: retunrs 0
  */
 
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	if (argc >= 0)
 	{
@@ -15,7 +15,7 @@ int main (int argc, char *argv[])
 	printf("%s\n", argv[0]);
 
 	return (0);
-	
+
 	}
 	return (1);
 
