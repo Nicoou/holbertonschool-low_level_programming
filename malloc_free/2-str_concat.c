e4fd573b4c4d75b8ff1f2a167598e0bacecb2e8a
root <nf9600372@gmail.com>
2023-03-14 10:05:09 -0700

    task 2

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+char *str_concat(char *s1, char *s2)
+{
+	unsigned int a = 0, b = 0, c1 = 0, c2 = 0;
+	char *str;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	for (; s1[a]; a++)
+		;
+	for (; s2[b]; b++)
+		;
+	str = malloc(sizeof(char) * ((a + b) + 1));
+
+	if (str == NULL)
+		return (NULL);
+
+	for (; s1[c1] != '\0'; c1++)
+		str[c1] = s1[c1];
+
+	for (; s2[c2]; c2++)
+		str[c1 + c2] = s2[c2];
+	
+	str[c1 + c2] = '\0';
+
+	return (str);
+
+
+}
