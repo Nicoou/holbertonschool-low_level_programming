40cf6cb9d45c777a28d11366801eed97202c9258
root <nf9600372@gmail.com>
2023-03-13 19:36:45 -0700

    task 1

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - return a pointer
+ * @str: This parameter is a pointer to a string of characters
+ * @leng -  Variable to store the length of the input string
+ *
+ * Return: srt.
+ */
+
+char *_strdup(char *str)
+{
+	int x;
+	int leng = 0;
+
+	char *srt = malloc(sizeof(char) * (leng + 1));
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	while (str[leng] != '\0')/*Calcular la longitud de la cadena de entrada*/
+	{
+		leng++;
+	}
+
+	for (x = 0; x <= leng; x++)
+	{
+		srt[x] = str[x];
+	}
+
+	return (srt);
+}
