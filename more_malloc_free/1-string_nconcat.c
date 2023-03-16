6fe715178a292499a977d94b8d860597c76ee068
root <nf9600372@gmail.com>
2023-03-16 07:57:26 -0700

    task 1

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int a = 0, b = 0, na, no;
+	char *array = NULL;
+
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	for (; s1[a]; a++)
+		;
+
+	for (; s2[b]; b++)
+		;
+
+	array = (char *)malloc((a + n + 1));
+
+	if (array == NULL)
+		return (NULL);
+
+	for (na = 0; na < (a + n); na++)
+	{
+		if (na < a)
+		{
+			array[na] = s1[na];
+		}
+		else if (na >= a)
+		{
+			array[na] = s2[no];
+			no++;
+		}
+
+	}
+
+	array[na] = '\0';
+	return(array);	
+}
