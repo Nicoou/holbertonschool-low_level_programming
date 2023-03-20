b501a2e2d324fec33c28cbd634a8fb26ab0d68d4
root <nf9600372@gmail.com>
2023-03-20 13:46:01 -0700

    task 3

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -14,9 +14,14 @@ struct dog
 	float age;
 	char *owner;
 
-} dog_t;
-
+};
+	/*task 1*/
 void init_dog(struct dog *d, char *name, float age, char *owner);
+
+	/*task 2*/
 void print_dog(struct dog *d);
+
+	/*task 3*/
+typedef struct dog dog_t;
 
 #endif
