6d12454b04c49a4eb1a13b9ccb43cca45555df70
root <nf9600372@gmail.com>
2023-03-20 14:41:48 -0700

    task 5

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,53 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * new_dog - Creates a new dog structure with new data
+ * @name : name's dog
+ * @age : age's dog
+ * @owner : owner
+ *
+ * Return: new dog location
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	int save_owner, save_name, i;
+
+	dog_t *point;/*point = new_dog*/
+
+	point = malloc(sizeof(dog_t));
+		if (point == NULL)
+			return (NULL);
+	/*get the length of the character strings*/
+	save_owner = strlen(owner);
+	save_name = strlen(name);
+	point->name = malloc(save_name + 1);
+	{
+		if (point->name == NULL)
+		{
+			free(point);
+			return (NULL);
+		}
+	}
+	point->owner = malloc(save_owner + 1);
+	{
+		if (point->owner == NULL)
+		{
+			free(point->name);
+			free(point);
+			return (NULL);
+		}
+	}
+	for (i = 0; i < save_name; i++)
+		point->name[i] = name[i];
+
+	point->name = '\0';
+	for (i = 0; i < save_owner; i++)
+		point->owner[i] = owner[i];
+
+	point->owner = '\0';
+	point->age = age;
+	return (point);
+}
