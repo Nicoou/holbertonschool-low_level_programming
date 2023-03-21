7cbc8a86e321b46aa81667fba2d22672ef0a66e3
root <nf9600372@gmail.com>
2023-03-21 16:12:21 -0700

    task 1

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - Print a name.
+ *
+ * @name: String to print.
+ * @f: Function pointer that return nothing and contain one param char pointer.
+ * Return: void.
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
