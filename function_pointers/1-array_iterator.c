c7a31a63778f7158c9f272f0236e62fa39733648
root <nf9600372@gmail.com>
2023-03-21 16:30:35 -0700

    task 1

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - Executed a parameter on each element of an array.
+ *
+ * @array: Data structure.
+ * @size: Size of the array.
+ * @action: function passed by parameters.
+ *
+ * Return: void.
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	for (i = 0; i < size; i++)
+	{
+		(*action)(array[i]);
+	}
+}
