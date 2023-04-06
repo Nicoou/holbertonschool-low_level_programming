52d1e896ac2efdc0a39e53bc4364d318ceabc938
root <nf9600372@gmail.com>
2023-04-06 13:07:41 -0700

    task 4

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_list - free list
+ *
+ * @head: list a free
+ */
+void free_list(list_t *head)
+{
+	if (head != NULL)
+	{
+		if (head->str != NULL)
+		{
+			free(head->str);
+		}
+		free_list(head->next);
+		free(head);
+	}
+}
