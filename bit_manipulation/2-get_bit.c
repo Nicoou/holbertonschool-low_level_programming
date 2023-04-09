cb8eedefbd06bab3df4780665dac73fa1fddebcb
root <nf9600372@gmail.com>
2023-04-09 14:15:17 -0700

    task 2

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -10,29 +10,22 @@ unsigned int lenbit(unsigned long int n);
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned int i = n;
+	unsigned long int mask;
 
-	if (lenbit(4294967295) < index)
+	if (index >= sizeof(unsigned long int) * 8 - 10)
+	{
 		return (-1);
-	if (lenbit(n) < index)
+	}
+
+	mask = 1UL << index;
+	
+	if (n & mask)
+	{
+		return (1);
+	}
+
+	else
+	{
 		return (0);
-	if (n > 1 && index > 0)
-		i = get_bit(n / 2, index - 1);
-
-	return (i & 1);
-}
-/**
- * lenbit - len the quantity of bit of number.
- *
- * @n: number in decimals.
- * Return: returns the quantity.
- */
-unsigned int lenbit(unsigned long int n)
-{
-	unsigned int i = 1;
-
-	if (n > 1)
-		i = 1 + lenbit(n / 2);
-
-	return (i);
+	}
 }
